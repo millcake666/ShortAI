fbc000ecdde4676371ed58bd3f106bfe7b0f5111
synikolaev <synikolaev@inno.tech>
2023-02-02 15:35:16 +0300

    orval code gen

--- orval.config.js
+++ orval.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'swagger-api': {
-    input: `/docs/api-docs.json`, // положите сюда свой сваггер/openapi конфиг
+    input: `http://151.248.122.104:8000/openapi.json`, // положите сюда свой сваггер/openapi конфиг
     output: {
       target: './packages/short-ai-app/src/features/api/generated/endpoints.ts',
       schemas: './packages/short-ai-app/src/features/api/generated/models',
