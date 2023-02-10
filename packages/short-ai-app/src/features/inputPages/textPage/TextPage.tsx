4a5d54cc6146648303de5a9603d774f48db6c70d
synikolaev <synikolaev@inno.tech>
2023-02-10 03:23:24 +0300

    wip

--- packages/short-ai-app/src/features/inputPages/textPage/TextPage.tsx
+++ packages/short-ai-app/src/features/inputPages/textPage/TextPage.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Button, TextField, Typography } from '@mui/material'
 import styled from '@emotion/styled'
+import { Button, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+import { useCreateTaskTasksPost } from '../../api/generated/endpoints'
 import { Flex, Pad, Spacer } from '../../primitives'
 import { InputBar } from '../inputBar/InputBar'
-import { useCreateTaskTasksPost } from '../../api/generated/endpoints'
 
 export const TextPage = () => {
   return (
